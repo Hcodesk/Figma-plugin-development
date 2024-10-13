a7dc72335a32d42afe21a148d85e72663bf99cfe
unknown <lhzik20@gmail.com>
2024-10-13 08:47:36 +0100

    new v

--- first-figma-plugin/code.ts
+++ first-figma-plugin/code.ts
@@ -2,4 +2,10 @@ figma.showUI(__html__)
 
 figma.ui.resize(500,500)
 
-figma.ui.onmessage = pluginMessage => {}
+figma.ui.onmessage = pluginMessage => {
+    console.log(pluginMessage.name)
+    console.log(pluginMessage.username)
+    console.log(pluginMessage.description)   
+    console.log(pluginMessage.darkModeState)    
+    console.log(pluginMessage.imageVariant)   
+}
